02de14702b8ae154f80fc6255973347b9de8453e
syvixor <syvixor@proton.me>
2025-07-08 18:18:09 +0100

    update: enhance seo

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,11 @@
 <script lang="ts" setup>
 const { data } = await useStoryblokApi().getStories({ version: "published" });
-const seo = {
-    title: "Exploring Technology, News, and Innovation",
-    description: "Stay updated with the latest in tech, innovation, and global digital trends.",
-    url: "https://blog.syvixor.com",
-    image: "https://blog.syvixor.com/og.png"
-}
 </script>
 
 <template>
-    <Seo :title="seo.title" :description="seo.description" :url="seo.url" :image="seo.image" />
+    <Seo title="Exploring Technology, News, and Innovation"
+        description="Stay updated with the latest in tech, innovation, and global digital trends."
+        url="https://blog.syvixor.com"
+        image="https://a.storyblok.com/f/285509672342890/1200x628/2848753c72/f3875f2c-98c3-4a4e-8c30-18529b1f68e5.webp" />
     <Posts :data="data.stories" />
 </template>
--- app/pages/post/[slug].vue
+++ app/pages/post/[slug].vue
@@ -1,14 +1,9 @@
 <script lang="ts" setup>
 const { value } = await useAsyncStoryblok("/post/" + useRoute().params.slug, { version: "draft" });
-const seo = {
-    title: value.content.caption,
-    description: value.content.summary,
-    url: "https://blog.syvixor.com/" + value.full_slug,
-    image: value.content.cover.filename
-}
 </script>
 
 <template>
-    <Seo :title="seo.title" :description="seo.description" :url="seo.url" :image="seo.image" />
+    <Seo :title="value.content.caption" :description="value.content.summary"
+        :url="`https://blog.syvixor.com/${value.full_slug}`" :image="value.content.cover.filename" />
     <StoryblokComponent :blok="value.content" />
 </template>
