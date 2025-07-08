20c40cf91dfbbd40fcd42600ab33bcff5f5dc986
syvixor <syvixor@proton.me>
2025-07-08 17:32:10 +0100

    update: enhance page mounting

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,15 @@
 <script lang="ts" setup>
+const isReady = ref(false);
+onMounted(() => isReady.value = true);
 </script>
 
 <template>
-    <div class="space-y-8 py-4 px-6">
+    <div class="space-y-8 py-4 px-6" v-show="isReady">
         <Header />
         <slot />
         <Footer />
+    </div>
+    <div class="flex justify-center items-center h-screen" v-show="!isReady">
+        <UButton label="Loading" color="neutral" variant="link" size="xl" loading />
     </div>
 </template>
